c8e3dc1ed998034da4be850a4a7b838be93cc6c4
Nikhil Gandhi <164669789+nikhilgandhi08@users.noreply.github.com>
2024-07-26 22:06:38 +0530

    Files Added

---
+++ Java/Package4/Package4/Area1.java
@@ -0,0 +1,21 @@
+package Package4;
+
+public class Area1 {
+	
+		
+		public float circle(float r){
+			return 3.14f*r*r;
+		}
+		
+		public float triangle(float b, float h) {
+			return 0.5f*b*h;
+		}
+		
+		public float rectangle(float l, float b) {
+			return l*b;
+		}
+	
+	
+	
+	
+}
---
+++ Java/Package4/Package4/operation.java
@@ -0,0 +1,23 @@
+package Package4;
+
+public class operation {
+	public void addData(int a, int b) {
+		int add=a+b;
+		System.out.println("Addition is: "+add);
+	}
+	
+	public void subData(int a, int b) {
+		int sub=a-b;
+		System.out.println("Substraction is: "+sub);
+	}
+	
+	public void mulData(int a, int b) {
+		int mul=a*b;
+		System.out.println("Multiplication is: "+mul);
+	}
+	
+	public void divData(int a, int b) {
+		int div=a/b;
+		System.out.println("Division is: "+div);
+	}
+}
